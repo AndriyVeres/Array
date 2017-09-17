6c7f0806580350407dba47708f40344e86f5cd1f
Andriy Veres <andriy-pilot@ukr.net>
2017-09-17 15:51:32 +0300

    Added README

--- Array.hpp
+++ Array.hpp
@@ -7,8 +7,27 @@
 namespace Array
 {
 
+//--------------ABBREVIATIONS---------------------//
+/*
+pArr           - pointer to array
+SIZE           - array size
+iSymbolWidth   - width of symbols in showing order for leveling for each array element
+cSpaceSymbol   - symbol, that will be used as spacing symboll between two array elements
+iDivider       - divider as used as upper threshold for pseudo-random numbers  generation
+Key            - searching key
+iMax           - maximum index of searching (size of array as well)
+iMin           - minimum index of seearching (0 as well)
+(*pOrder)(T,T) - ASC or DESC
+*/
 //--------------MANIPULATION FUNCTIONS------------//
 
+/*
+This functions used for manipulating order of 
+sorting in sorting functions as logic eqiuation 
+return value result
+*/
+
+//Ascending order
 template <typename T>
 bool ASC(T a1, T a2)
 {
@@ -16,6 +35,7 @@ bool ASC(T a1, T a2)
   else        return false;
 }
 
+//Descending order
 template <typename T>
 bool DESC(T a1, T a2)
 {
@@ -100,30 +120,30 @@ void Random(float *pArr,const int SIZE, int iDivider)
 }
 
 //----------------SEARCH FUNCTIONS-------------------//
-//function: binary search algorithm on input array
+//function: binary search algorithm on input array, returns position of key in the array
 template <typename T>
-int BinarySeacrh(T nKey,T * pArr,unsigned int iMax,unsigned int iMin)
+int BinarySeacrh(T Key,T * pArr,unsigned int iMax,unsigned int iMin)
 {
   unsigned int iMiddle = ( iMax + iMin ) / 2;
 
   if(iMin + 1 == iMax)
     {
 
-      if(nKey == pArr[iMax])
+      if(Key == pArr[iMax])
         return iMax;
-      else if(nKey == pArr[iMin])
+      else if(Key == pArr[iMin])
         return iMin;
       else
         return -1;
     }
-  else if (nKey == pArr[iMiddle])
+  else if (Key == pArr[iMiddle])
     return iMiddle;
 
-  else if(nKey <= pArr[iMiddle])
-    return BinarySeacrh(nKey,pArr,iMiddle,iMin);
+  else if(Key <= pArr[iMiddle])
+    return BinarySeacrh(Key,pArr,iMiddle,iMin);
 
-  else if(nKey >= pArr[iMiddle])
-    return BinarySeacrh(nKey,pArr,iMax,iMiddle);
+  else if(Key >= pArr[iMiddle])
+    return BinarySeacrh(Key,pArr,iMax,iMiddle);
 
 }
 
@@ -146,6 +166,7 @@ void BubbleSort (T *pArr,const int SIZE, bool (*pOrder)(T,T))
         }
 }
 
+//function: optimized bubble sort algorithm on input array
 template <typename T>
 void BubbleSortOptimized (T *pArr,const int SIZE,bool (*pOrder)(T,T))
 {
---
+++ README.md
@@ -0,0 +1,70 @@
+# Some usefull functions for working with arrays
+
+### namespace _Array_
+
+### ABBREVIATIONS
+
+_pArr_   - pointer to array
+
+_SIZE_   - array size
+
+_iSymbolWidth_   - width of symbols in showing order for leveling for each array element
+
+_cSpaceSymbol_  - symbol, that will be used as spacing symboll between two array elements
+
+_iDivider_      - divider as used as upper threshold for pseudo-random numbers  generation
+
+_Key_            - searching key
+
+_iMax_           - maximum index of searching (size of array as well)
+
+_iMin_           - minimum index of seearching (0 as well)
+
+_ASC_ - ascending order
+_DESC_ - descending order
+
+
+### SHOW AND FILLING FUNCTION
+
+
+**void Show(const T *pArr,const int SIZE,unsigned int iSymbolWidth,char cSpaceSymbol);**
+>*function, that show input array*
+
+**void Random(T *pArr,const int SIZE, int iDivider);**
+>*function: pseudo-randow fill the input array*
+ 
+### SEARCH FUNCTIONS
+
+**int BinarySeacrh(T Key,T * pArr,unsigned int iMax,unsigned int iMin);**
+>*function: binary search algorithm on input array, return position of Key in the array*
+  
+### SORTING FUNCTIONS
+
+**void BubbleSort (T *pArr,const int SIZE, bool (*pOrder)(T,T));**
+>*bubble sort algorithm*
+  
+**void BubbleSortOptimized (T *pArr,const int SIZE,bool (*pOrder)(T,T));**
+>*Optimized bubble sort algorithm*
+
+**void SelectionSort(T * pArr,const unsigned int SIZE, bool (*pOrder)(T,T));**
+>*selection sort algorithm*
+
+### EXAMPLE
+
+	#include <iostream>
+	#include "Array.hpp"
+	
+	using namespace std;
+
+	int main()
+	{	
+    	const int SIZE = 10;                             //size of array
+    	int Arr[SIZE];                                   //creating array of int values
+
+    	Array::Random(Arr,SIZE,5);                       //random array filling
+
+    	Array::BubbleSortOptimized(Arr,SIZE,Array::ASC); //Optimized bubble sort in ascending order
+    	Array::Show(Arr,SIZE,3,' ');                     //Array show
+	
+    	return 0;
+	}
